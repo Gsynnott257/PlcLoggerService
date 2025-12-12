ef06afbde23a7e35e147c222bd56742a11ac91fc
Graham Synnott <gsynnott@linamar.com>
2025-12-12 11:20:34 -0500

    Add project files.

---
+++ PlcLoggerService/Plc/LibPlcTagReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlcLoggerService.Plc
+{
+    internal class LibPlcTagReader
+    {
+    }
+}
---
+++ PlcLoggerService/Services/SqlWriter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlcLoggerService.Services
+{
+    internal class SqlWriter
+    {
+    }
+}
